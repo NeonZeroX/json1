dd0e3bede00afc15aa2ffb470425bf0e5594abd9
NeonZeroX <80101013+NeonZeroX@users.noreply.github.com>
2021-12-07 04:00:15 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,31 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <button id="btnjs1" > JSON 1</button>
-    Name :<span id ="fname"><span><br/>
-    Sname :<span id="lname"></span>
+    <button id="btnJSON1"> JSON 1 </button>
+    Name : <span id="fname"></span> <br/>
+    Sname: <span id="lname"></span>
 </body>
-
 <script>
-
-    function loadJS(){
-        var url = "https://cj-android-demon.herokuapp.com/json1.php"
-        $.get(url,(data,status) => {
-            console.log(data);
-            var jData = JSON.parse(data);
-            console.log(data);
-        });
-
-    }
-    $(() => {
-        $("btnjs1").click(loadJS());
+function loadJSON(){
+    var url = "https://cj-android-demon.herokuapp.com/json1.php";
+    $.get(url, (data, status)=>{
+        console.log(data);
+        var jData = JSON.parse(data);
+        console.log(jData.fname);
+        console.log(jData.lname);
+        $("#fname").text(jData.fname);
+        $("#lname").text(jData.lname);
     });
+}
+$(()=>{
+    $("#btnJSON1").click(loadJSON);
+});
 </script>
-
 </html>
